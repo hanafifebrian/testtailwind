1d79afe5216ab75dc90d814d29d6b29df8894d2c
Hanafi Febriansyah <hanafifebrian87@gmail.com>
2023-11-05 14:42:39 +0700

    connect to database and emali(formspree)

--- namSa.php
+++ namSa.php
@@ -1,25 +1,39 @@
 <?php
-// Panggil variabel dari form
-$nama = $_POST['nama'];
-$saran = $_POST['saran'];
-// Pengaturan koneksi ke database
-$host = "localhost"; // Ganti dengan string "localhost" untuk alamat server MySQL
-$user = "root";
-$pass = "";
-$db = "tailwind";
-// Membuka koneksi ke MySQL
-$koneksi = mysqli_connect($host, $user, $pass, $db);
-// Periksa koneksi
-if (!$koneksi) {
-    die("Koneksi ke database gagal: " . mysqli_connect_error());
-}
-// Query SQL untuk menyimpan komentar ke dalam tabel 
-$sql = "INSERT INTO komen (nama, saran) VALUES ('$nama', '$saran')";
-if (mysqli_query($koneksi, $sql)) {
-    echo "Komentar berhasil disimpan ke database.";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Pastikan data ada sebelum menyimpan ke database
+    if (isset($_POST['email']) && isset($_POST['message'])) {
+        $nama = $_POST['email'];
+        $saran = $_POST['message'];
+
+        // Pengaturan koneksi ke database
+        $host = "localhost";
+        $user = "root";
+        $pass = "";
+        $db = "tailwind";
+
+        // Membuka koneksi ke MySQL
+        $koneksi = mysqli_connect($host, $user, $pass, $db);
+
+        // Periksa koneksi
+        if (!$koneksi) {
+            die("Koneksi ke database gagal: " . mysqli_connect_error());
+        }
+
+        // Query SQL untuk menyimpan komentar ke dalam tabel
+        $sql = "INSERT INTO komen (nama, saran) VALUES ('$nama', '$saran')";
+
+        if (mysqli_query($koneksi, $sql)) {
+            echo "Komentar berhasil disimpan ke database.";
+        } else {
+            echo "Gagal: " . $sql . "<br>" . mysqli_error($koneksi);
+        }
+
+        // Tutup koneksi database
+        mysqli_close($koneksi);
+    } else {
+        echo "Data tidak lengkap. Pastikan email dan message tersedia.";
+    }
 } else {
-    echo "Gagal: " . $sql . "<br>" . mysqli_error($koneksi);
+    echo "Metode request bukan POST.";
 }
-// Tutup koneksi database
-mysqli_close($koneksi);
 ?>
