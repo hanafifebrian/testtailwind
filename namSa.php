61d0d88d7b7486ff94d9a98f8629a7549b8eba82
Hanafi Febriansyah <hanafifebrian87@gmail.com>
2023-11-05 09:33:13 +0700

    animasi update

--- namSa.php
+++ namSa.php
@@ -2,30 +2,24 @@
 // Panggil variabel dari form
 $nama = $_POST['nama'];
 $saran = $_POST['saran'];
-
 // Pengaturan koneksi ke database
 $host = "localhost"; // Ganti dengan string "localhost" untuk alamat server MySQL
 $user = "root";
 $pass = "";
 $db = "tailwind";
-
 // Membuka koneksi ke MySQL
 $koneksi = mysqli_connect($host, $user, $pass, $db);
-
 // Periksa koneksi
 if (!$koneksi) {
     die("Koneksi ke database gagal: " . mysqli_connect_error());
 }
-
-// Query SQL untuk menyimpan komentar ke dalam tabel "komentar" (pastikan tabel ini sudah ada dalam database "tailwind")
+// Query SQL untuk menyimpan komentar ke dalam tabel 
 $sql = "INSERT INTO komen (nama, saran) VALUES ('$nama', '$saran')";
-
 if (mysqli_query($koneksi, $sql)) {
     echo "Komentar berhasil disimpan ke database.";
 } else {
     echo "Gagal: " . $sql . "<br>" . mysqli_error($koneksi);
 }
-
 // Tutup koneksi database
 mysqli_close($koneksi);
 ?>
